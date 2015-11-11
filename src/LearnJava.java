57bbe0e343630f964685109fb3ae51aaf5e538a8
Jonathan Pham <jon.pham@gmail.com>
2015-11-10 22:15:13 -0800

    Add Guessing Game v1. Movie Test Driver.

---
+++ src/LearnJava.java
@@ -0,0 +1,24 @@
+/* Java Tutorials
+ * Coding Style from  Google Java Style (2015.11)
+ * https://google.github.io/styleguide/javaguide.html
+ */ 
+
+// Class Definition
+/* <access-level> <def_type> <name> {} 
+ *
+ */
+
+// Class Method Definition
+
+public class LearnJava {
+	/* Special Class Method main()
+	 * Java searches for the main() method as the kickoff point of any java program.
+	 * There should never be more than one main() per executable.
+	 */
+	public static void main (String[] args) {
+		System.out.print("Hello World!\n");
+	}
+}
+
+public class LessonSelector {}
+public class LessonSuperClass {}
